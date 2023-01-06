e592dbda27c8ab2681fadd68a28ce852123f0492
Nick Garcia <nicholasgarcia2003@protonmail.com>
2023-01-06 16:58:36 -0700

    homepage main styling

---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+        <footer>
+            <ul>
+                <a href="physics.html"><li>Physics</li></a>
+                <a href="rowing.html"><li>Rowing & Fitness</li></a>
+                <a href="web-dev.html"><li>Software Projects</li></a>
+                <a href="compeng.html"><li>Hardware Projects</li></a>
+                <a href="contact.html"><li>Contact</li></a>
+            </ul>
+        </footer>
+    </body>
+</html>
